614b53846ccdc1d9f1e071c84e0d4a3d9f8462a5
HaileleulGirma <121661513+HaileleulGirma@users.noreply.github.com>
2025-05-06 00:12:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,32 +6,32 @@ import torch.nn.functional as F
 import praw
 import re
 
-# Load tokenizer and model
+
 model_name = "AdamCodd/tinybert-sentiment-amazon"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
 
 CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
 CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
-  # e.g. "http://localhost:8080/authorize_callback"
+  
 USER_AGENT = os.getenv("REDDIT_USER_AGENT")
 
-# Reddit API setup using PRAW
+
 reddit = praw.Reddit(
-    client_id=CLIENT_ID,  # Replace with your Reddit API client_id
-    client_secret=CLIENT_SECRET,  # Replace with your Reddit API client_secret
-    user_agent=USER_AGENT  # Replace with a user agent string
+    client_id=CLIENT_ID,  
+    client_secret=CLIENT_SECRET,  
+    user_agent=USER_AGENT  
 )
 
 
-# Function to scrape Reddit post text
+
 def get_reddit_post_text(post_url):
     try:
-        # Extract post ID from URL
+        
         post_id = re.findall(r"comments/([a-zA-Z0-9]+)/", post_url)[0]
         submission = reddit.submission(id=post_id)
 
-        # Get title and content (selftext)
+        
         title = submission.title
         text = submission.selftext
 
@@ -40,15 +40,15 @@ def get_reddit_post_text(post_url):
         return f"Error: Unable to retrieve the post. {str(e)}"
 
 
-# Gradio interface function for sentiment prediction
+
 def predict_sentiment(post_url):
-    # Get Reddit post text using the URL
+    
     post_text = get_reddit_post_text(post_url)
 
-    if "Error" in post_text:  # If there was an issue with the Reddit post retrieval
+    if "Error" in post_text:  
         return post_text
 
-    # Perform sentiment analysis on the post text
+    
     inputs = tokenizer(post_text, return_tensors="pt", truncation=True, padding=True)
     with torch.no_grad():
         outputs = model(**inputs)
@@ -60,12 +60,12 @@ def predict_sentiment(post_url):
     return f"Sentiment: {sentiment} (Confidence: {round(confidence * 100, 2)}%)\n\n{post_text}"
 
 
-# Gradio interface setup
+
 interface = gr.Interface(fn=predict_sentiment,
                          inputs=gr.Textbox(lines=2, placeholder="Enter a Reddit post URL..."),
                          outputs="text",
                          title="Sentiment Analyzer for Reddit",
                          description="Enter a Reddit post URL, and this tool will analyze its sentiment using TinyBERT.")
 
-# Launch Gradio interface
+
 interface.launch(share=True, server_name="0.0.0.0", server_port=7860, inbrowser=False)
